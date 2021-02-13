d475c7c7dd6784bb0d969182b98473e7de62a89e
Ahmad Arsyel <arsyel@agate.id>
2021-02-13 13:35:23 +0700

    register new assets

--- src/ts/library/AssetGameplay.ts
+++ src/ts/library/AssetGameplay.ts
@@ -7,5 +7,20 @@ export const Assets = {
 		url: 'img/player/img_player_raft.png',
 		type: AssetType.STATIC
 	},
+	obs_boulder: {
+		key: 'obs_boulder',
+		url: 'img/obstacles/img_obs_boulder.png',
+		type: AssetType.STATIC
+	},
+	obs_plank: {
+		key: 'obs_plank',
+		url: 'img/obstacles/img_obs_plank.png',
+		type: AssetType.STATIC
+	},
+	obs_sharp: {
+		key: 'obs_sharp',
+		url: 'img/obstacles/img_obs_sharp.png',
+		type: AssetType.STATIC
+	},
 
 };
