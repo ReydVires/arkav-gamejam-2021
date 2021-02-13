2d33ef6c77928a246e28b604d4fd847ea91d64dc
Ahmad Arsyel <arsyel@agate.id>
2021-02-13 15:08:11 +0700

    add swipe behavior for obstacle

--- src/ts/scenes/gameplay/obstacle/ObstacleView.ts
+++ src/ts/scenes/gameplay/obstacle/ObstacleView.ts
@@ -24,7 +24,7 @@ export class ObstacleView implements BaseView {
 	};
 
 	private _maxTimeToSpawn: number;
-	private _obstacleSprites: ArcadeSprite[];
+	private _obstacleSprites: Phaser.Physics.Arcade.Sprite[];
 
 	constructor (private _scene: Phaser.Scene) {
 		this.screenUtility = ScreenUtilController.getInstance();
@@ -33,7 +33,7 @@ export class ObstacleView implements BaseView {
 		this._obstacleSprites = [];
 	}
 
-	get obstacles (): ArcadeSprite[] {
+	get obstacles (): Phaser.Physics.Arcade.Sprite[] {
 		return this._obstacleSprites;
 	}
 
@@ -42,13 +42,54 @@ export class ObstacleView implements BaseView {
 	}
 
 	private getAssetTypeKey (): string {
-		const assetKeys = [
+		const assetKeys = [ // TODO: Define with object data property
 			Assets.obs_boulder.key,
 			Assets.obs_plank.key,
 			Assets.obs_sharp.key,
 		];
 		const randomPick = Math.floor(Math.random() * assetKeys.length);
 		return assetKeys[randomPick];
+	}
+
+	private setInteractive (gameObject: Phaser.Physics.Arcade.Sprite): void {
+		gameObject.setInteractive({ useHandCursor: true });
+
+		const assetType = gameObject.getData(DataProps.assetType) as string;
+		const dataProps = {
+			counter: "counter"
+		};
+		switch (assetType) {
+		case Assets.obs_boulder.key:
+			gameObject.on("pointerup", () => {
+				let prevCounter: number = gameObject.getData(dataProps.counter) ?? 0;
+				if (++prevCounter >= 2) {
+					gameObject.setData(dataProps.counter, 0);
+					this.deactiveGameObject(gameObject);
+					return;
+				}
+				gameObject.setData(dataProps.counter, prevCounter);
+			});
+			break;
+		case Assets.obs_plank.key:
+			const inputPlugin = this._scene.input;
+			inputPlugin.setDraggable(gameObject);
+			inputPlugin.dragDistanceThreshold = 32 * gameObject.getData(DataProps.displayPercentage);
+
+			gameObject.on("pointerdown", () => {
+				gameObject.setVelocity(0);
+			});
+			gameObject.on("drag", (p: Phaser.Input.Pointer) => {
+				gameObject.x = p.x;
+			});
+			gameObject.on("dragend", () => this.deactiveGameObject(gameObject));
+			break;
+		default:
+			gameObject.once("pointerup", () => {
+				// TODO: Play particle
+				this.deactiveGameObject(gameObject);
+			});
+			break;
+		}
 	}
 
 	private spawnObstacle (displayPercentage: number, edges: number[], assetType: string): void {
@@ -69,35 +110,38 @@ export class ObstacleView implements BaseView {
 		);
 		obstacle.gameObject.setVelocityY(speedRelative * displayPercentage);
 
-		this._obstacleSprites.push(obstacle);
+		this.setInteractive(obstacle.gameObject);
+
+		this._obstacleSprites.push(obstacle.gameObject);
 	}
 
-	reuseObstacle (obstacle: ArcadeSprite): void {
-		obstacle.gameObject.setActive(true);
-		obstacle.gameObject.enableBody(false, 0, 0, true, true);
+	private reuseObstacle (gameObject: Phaser.Physics.Arcade.Sprite): void {
+		gameObject.setActive(true);
+		gameObject.enableBody(false, 0, 0, true, true);
 
-		const displayPercentage = obstacle.gameObject.getData(DataProps.displayPercentage) as number;
-		obstacle.transform.setToScaleDisplaySize(displayPercentage);
-
-		const [left, right, top, bottom] = obstacle.gameObject.getData(DataProps.backgroundEdges) as number[];
-		obstacle.gameObject.setPosition(
-			Phaser.Math.Between(left + (obstacle.transform.displayWidth / 2), right - (obstacle.transform.displayWidth / 2)),
-			bottom + (obstacle.transform.displayHeight / 2)
+		const [left, right, top, bottom] = gameObject.getData(DataProps.backgroundEdges) as number[];
+		gameObject.setPosition(
+			Phaser.Math.Between(left + (gameObject.displayWidth / 2), right - (gameObject.displayWidth / 2)),
+			bottom + (gameObject.displayHeight / 2)
 		);
 
 		const speedRelative = -190;
-		obstacle.gameObject.setVelocityY(speedRelative * displayPercentage);
+		const displayPercentage = gameObject.getData(DataProps.displayPercentage) as number;
+		gameObject.setVelocityY(speedRelative * displayPercentage);
+
+		this.setInteractive(gameObject);
 	}
 
-	deactiveObstacle (gameObject: Phaser.Physics.Arcade.Sprite): void {
+	deactiveGameObject (gameObject: Phaser.Physics.Arcade.Sprite): void {
 		gameObject.setVelocity(0).disableBody(true, true);
+		gameObject.removeAllListeners();
 		gameObject.setActive(false);
 	}
 
 	create (displayPercentage: number, edges: number[]): void {
 		this.event.on(EventNames.onSpawn, () => {
 			const assetType = this.getAssetTypeKey();
-			const obstacle = this._obstacleSprites.find((obstacle) => !obstacle.gameObject.active && (obstacle.gameObject.getData(DataProps.assetType) === assetType));
+			const obstacle = this._obstacleSprites.find((obstacle) => !obstacle.active && (obstacle.getData(DataProps.assetType) === assetType));
 			if (obstacle) {
 				this.reuseObstacle(obstacle);
 				return;
