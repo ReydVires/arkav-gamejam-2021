3739fc3f4447a701887afa017419249a969ee17f
Ahmad Arsyel <arsyel@agate.id>
2021-02-13 07:39:23 +0700

    implement bg and player

--- src/ts/scenes/gameplay/GameplaySceneController.ts
+++ src/ts/scenes/gameplay/GameplaySceneController.ts
@@ -5,6 +5,8 @@ import { EventNames, GameplaySceneView } from "./GameplaySceneView";
 import { CameraController } from "./camera/CameraController";
 import { DebugController } from "./debug/DebugController";
 import { SceneInfo } from "../../info/SceneInfo";
+import { PlayerController } from "./player/PlayerController";
+import { BackgroundController } from "./background/BackgroundController";
 
 type OnCreateFinish = (...args: unknown[]) => void;
 
@@ -14,6 +16,8 @@ export class GameplaySceneController extends Phaser.Scene {
 	audioController: AudioController;
 	cameraController: CameraController;
 	debugController: DebugController;
+	playerController: PlayerController;
+	bgController: BackgroundController;
 
 	constructor () {
 		super({key: SceneInfo.GAMEPLAY.key});
@@ -24,16 +28,22 @@ export class GameplaySceneController extends Phaser.Scene {
 		this.audioController = AudioController.getInstance();
 		this.cameraController = new CameraController(this);
 		this.debugController = new DebugController(this);
+		this.bgController = new BackgroundController(this);
+		this.playerController = new PlayerController(this);
 
 		this.cameraController.init();
 		this.debugController.init();
+		this.bgController.init();
+		this.playerController.init(
+			this.bgController.displayPercentage()
+		);
 
 		this.onPlaySFXClick(() => this.audioController.playSFX(Audios.sfx_click.key));
 		this.onClickRestart(() => {
 			this.scene.start(SceneInfo.TITLE.key);
 		});
-		this.onCreateFinish((uiView) => {
-			this.cameraController.registerGameobjectInCamera(uiView as Phaser.GameObjects.Container, CameraKeyList.UI);
+		this.onCreateFinish((uiView: Phaser.GameObjects.Container) => {
+			this.cameraController.registerGameobjectInCamera(uiView, CameraKeyList.UI);
 			this.debugController.show(true);
 		});
 	}
@@ -47,6 +57,8 @@ export class GameplaySceneController extends Phaser.Scene {
 			this.view.event.emit(EventNames.onClickRestart);
 		}
 		this.cameraController.update(time, dt);
+		this.bgController.update(time, dt);
+		this.playerController.update(time, dt);
 	}
 
 	onPlaySFXClick (event: Function): void {
