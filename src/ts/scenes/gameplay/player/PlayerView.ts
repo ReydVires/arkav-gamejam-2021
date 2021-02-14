3cdb3fe35ade3f95a5b2f33231eae58636cffb6e
Ahmad Arsyel <arsyel@agate.id>
2021-02-14 14:49:07 +0700

    change to default player sprite

--- src/ts/scenes/gameplay/player/PlayerView.ts
+++ src/ts/scenes/gameplay/player/PlayerView.ts
@@ -27,7 +27,7 @@ export class PlayerView implements BaseView {
 	create (displayPercentage: number, edges: number[]): void {
 		const { centerX, height } = this.screenUtility;
 		const [left, right, top, bottom] = edges;
-		this._sprite = new ArcadeSprite(this._scene, 0, 0, Assets.player_idle.key);
+		this._sprite = new ArcadeSprite(this._scene, 0, 0, Assets.player_raft.key);
 		this._sprite.transform.setToScaleDisplaySize(displayPercentage);
 		this._sprite.gameObject.setPosition(centerX, top + this._sprite.transform.displayHeight + (height * 0.15));
 	}
