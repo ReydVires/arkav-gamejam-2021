0a7c8b55b49fa92b1aa1eff0b484618f365d953b
Ahmad Arsyel <arsyel@agate.id>
2021-02-14 17:48:36 +0700

    add pooling backgrounds

--- src/ts/scenes/gameplay/background/BackgroundController.ts
+++ src/ts/scenes/gameplay/background/BackgroundController.ts
@@ -12,14 +12,22 @@ export class BackgroundController {
 		this._view.create();
 	}
 
-	update (time: number, dt: number): void {}
-
 	displayPercentage (): number {
 		return this._view.displayHeightRatio;
 	}
 
 	getEdge (): number[] {
-		return this._view.getEdge;
+		return this._view.edge;
+	}
+
+	update (time: number, dt: number): void {
+		this._view.backgrounds.forEach((riverBg) => {
+			riverBg.gameObject.y -= (dt * 0.45) * this._view.displayHeightRatio;
+			const { y: bottom } = riverBg.gameObject.getBottomCenter();
+			if (bottom < 0) {
+				riverBg.gameObject.y += (riverBg.gameObject.displayHeight * 2);
+			}
+		});
 	}
 
 }
